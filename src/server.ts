34c53eb57eb13f7ca9367f85fc07014b58f166f5
DevDaniel <danielm@raciti.com.mx>
2023-06-12 19:01:01 -0600

    add ssl

--- src/server.ts
+++ src/server.ts
@@ -29,21 +29,18 @@ class Server {
         this.startServer();
     }
 
-    public startServer() {
+    private startServer() {
         const key = fs.readFileSync('./ssl/devdcm.com_private_key');
         const cert = fs.readFileSync('./ssl/mi_certificado.crt');    //  Set up routes (and middlewares if we had any)
 
-        const server = https.createServer({ key, cert }, this.app);     
-        server.listen(this.port, () => {
-            console.log(`Listo por el puerto ${this.port}`);
-        });
+        https.createServer({ key, cert }, this.app);
       }
 
-    // listen() {
-    //     this.app.listen(this.port, () => {
-    //         console.log(`Listo por el puerto ${this.port}`)
-    //     });
-    // }
+     listen() {
+         this.app.listen(this.port, () => {
+             console.log(`Listo por el puerto ${this.port}`)
+         });
+     }
 
     async dbConnection() {
         try {
